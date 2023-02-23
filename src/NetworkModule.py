4dcebf25233d46d1fb3abe5be863ebde36351d52
Nathi1235 <eiternathanael@gmail.com>
2023-02-23 16:51:06 +0100

    Comment Code

--- src/NetworkModule.py
+++ src/NetworkModule.py
@@ -3,16 +3,20 @@ HTL-Blackjack - Networking Module
 Author: Nathanael Eiter
 Date: 23.02.2023
 """
+#import libraries
 import socket
 import pickle
 
+#parent network class
 class Network:
     def __init__(self):
-        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     #create socket
     
-    def send(self,data:dict):
+    #data send function
+    def send(self,data):
         self.connection.send(pickle.dumps(data))
     
+    #data recieve function
     def recieve(self,conn,buffer_size=1024):
         while True:
             data = conn[0].recv(buffer_size)
@@ -20,18 +24,21 @@ class Network:
                 break
             return pickle.loads(data)
 
+#client network class
 class Client_Net(Network):
     def __init__(self):
         super().__init__()
     
+    #function to connect to server
     def client_Connect(self,ip,port):
         self.connection.connect((ip,port))
 
-
+#server network class
 class Server_Net(Network):
     def __init__(self):
         super().__init__()
-        
+    
+    #function to listen for incomming connections from clients
     def server_Listen(self,ip,port):
         self.connection.bind((ip,port))
         self.connection.listen()
@@ -39,6 +46,7 @@ class Server_Net(Network):
         return client_connection, client_address
 
 
+#testing
 if __name__ == '__main__':
     import threading
     IP = "localhost"
@@ -47,12 +55,12 @@ if __name__ == '__main__':
     def serverTest():
         server = Server_Net()
         client1 = server.server_Listen(IP,PORT)
-        print(server.recieve(client1)["test"])
+        print(server.recieve(client1))
     t = threading.Thread(target=serverTest)
     t.start()
     
     client = Client_Net()
     client.client_Connect(IP,PORT)
-    packet = {"test":1235}
+    packet = "testing"
     client.send(packet)
     t.join()
