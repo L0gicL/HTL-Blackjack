3bea79dc2d21ac0e0a62bda06d6769446883618a
manueltrenk <m.trenker05@gmail.com>
2023-03-01 18:24:57 +0100

    game loop started

--- src/GameLogicClient.py
+++ src/GameLogicClient.py
@@ -3,12 +3,13 @@ from NetworkModule import Client_Net
 
 client = Client_Net()
 client.client_Connect(IP,42069) #IP von Server
-client.send("Hello from Client!")
-print(client.recieve())
 
 mode = 0 #login/register-0, playing-1
 logorreg = #login-0 or register-1 from gui
 checkuser = 'no_user'
+playerbet = 0
+playeraction = 'action'
+cnt = 0
 
 while (mode == 0):
     client.send(mode) 
@@ -17,7 +18,7 @@ while (mode == 0):
         password = #password from gui
         client.send(username)
         client.send(hashlib.sha256(password.encode()).hexdigest())
-        client.recieve(checkuser)
+        checkuser = client.recieve()
         if (checkuser != 'no_user' or checkuser != 'wrong_password'):
             mode = 1
             break
@@ -34,4 +35,24 @@ while (mode == 0):
         else:
             #to gui password not same
 
+
 while (mode == 1):
+    if (cnt == 0):
+        playerbet = #amount from gui
+        client.send(playerbet)
+        cnt = 1
+
+    playerturn = client.recieve()
+    if (playerturn == 'turn'):
+        #gui: its your turn
+        playerturn = 'notturn'
+    
+    playeraction = #action from gui either draw or stand
+    client.send(playeraction)
+
+    cardinfo = client.recieve() #cardinfo how
+    #gui cardinfo
+
+    #when game ends set cnt=0
+    
+
